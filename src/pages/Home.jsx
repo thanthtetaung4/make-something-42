2fc095a6afc37940e1227a600696f86810260594
Thant Htet Aung <maung.good@gmail.com>
2024-06-25 22:18:57 +0800

    cookie added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,24 @@
 import React from 'react';
+import { useContext } from 'react';
+import { AuthContext } from '../components/AuthContext';
+import { useNavigate } from 'react-router-dom';
 
 const Home = () => {
-  console.log('====================================');
-  console.log("from home");
-  console.log('====================================');
+  const { logout } = useContext(AuthContext);
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    logout();
+    Cookies.remove('isLoggedIn'); // Clear the cookie
+    navigate('/login'); // Redirect to login page
+  };
+
   return (
     <div>
       <h1>Welcome to the Home Page!</h1>
       <p>This content is only accessible to logged-in users.</p>
+
+      <button onClick={handleLogout}>Logout</button>
     </div>
   );
 };
