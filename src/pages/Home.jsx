8d6e74ae5ae671e1177b853c6769cfae866dca58
korewadavid <korewadavid@gmail.com>
2024-06-26 05:28:28 +0000

    corrected colors and added 42logo

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -14,14 +14,14 @@ const Home = () => {
   };
 
   return (
-<body class="bg-black font-sans">
+<body class="bg-darkgrey font-sans">
     <div class="container mx-auto p-4">
-        <div class="header flex justify-between items-center bg-blue-500 p-4 rounded-lg shadow-md">
+        <div class="header flex justify-between items-center bg-white p-4 rounded-lg shadow-md">
             <div class="logo">
-                <p class="text-white text-2xl font-bold">42</p>
+                <img src="/src/assets/42_logo 1.svg" alt="42" width="50"/>
             </div>
             <div class="header-user">
-                <p class="text-white">User</p>
+                <p class="text-black">42 intra_id</p>
             </div>
         </div>
         <div class="flex mt-4">
@@ -39,15 +39,15 @@ const Home = () => {
                     </select>
                 </div>
                 <div class="searchbutton">
-                    <button class="w-full p-2 bg-blue-500 text-white rounded-lg">Search Your Peer</button>
+                    <button class="w-full p-2 bg-turquoise text-white rounded-lg">Search Your Peer</button>
                 </div>
             </div>
             <div class="body-block-right w-2/3 p-4 bg-white rounded-lg shadow-md">
-                <div class="peer-user flex flex-col items-start space-y-2">
+                <div class="peer-user flex flex-col items-start space-y-2 bg-turquoise">
                     <p class="bg-gray-200 p-2 rounded-lg">img</p>
                     <p>name</p>
                     <p>42 intra id</p>
-                    <button class="mt-2 p-2 bg-blue-500 text-white rounded-lg">See on intra</button>
+                    <button class="mt-2 p-2 bg-turquiose text-white rounded-lg">See on intra</button>
                 </div>
             </div>
         </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,12 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",],
   theme: {
     extend: {},
+    colors: {
+      'white': '#FFFFFF',
+      'turquoise': '#00BABC',
+      'darkgrey': '#292D39',
+      'black': '#1E1E1E'
+    },
   },
   plugins: [],
 }
